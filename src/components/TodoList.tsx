c88833afe6c59dbc8d134774ef1e4cc5c594923e
Kishkin Vitalii <dimpla30@gmail.com>
2023-07-14 19:21:22 +0300

    implemented the deletion and executed

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { AiFillDelete, AiFillEdit, AiFillCheckCircle } from "react-icons/ai";
 
 interface item {
   id: number;
@@ -31,6 +32,10 @@ export const TodoList: React.FC = () => {
     setInput("");
   };
 
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
   return (
     <div className="main">
       <h1 className="title">My Todo List</h1>
@@ -41,19 +46,35 @@ export const TodoList: React.FC = () => {
           value={input}
           onChange={e => setInput(e.target.value)}
         />
-        <button onClick={handleClick} disabled={input.length < 1}>
+        <button
+          className="btn"
+          onClick={handleClick}
+          disabled={input.length < 1}
+        >
           Add
         </button>
       </div>
-
+      <hr />
       <ul>
         {todos.map(todo => (
           <li
+            className="item"
             key={todo.id}
-            onClick={() => handleToggle(todo.id)}
+            // onClick={() => handleToggle(todo.id)}
             style={{ textDecoration: todo.completed ? "line-through" : "none" }}
           >
             {todo.text}
+            <div>
+              <button onClick={()=>handleToggle(todo.id)} className="edit-btn" style={{ color: "green" }}>
+                <AiFillCheckCircle />
+              </button>
+              <button className="edit-btn" style={{ color: "yellow" }}>
+                <AiFillEdit />
+              </button>
+              <button onClick={()=>handleDelete(todo.id)} className="del-btn" style={{ color: "red" }}>
+                <AiFillDelete />
+              </button>
+            </div>
           </li>
         ))}
       </ul>
