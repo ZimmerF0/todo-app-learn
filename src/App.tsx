27c212623c21ce622082e042ab69a520e59ffe44
Kishkin Vitalii <dimpla30@gmail.com>
2023-07-16 11:40:34 +0300

    add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+
 import { TodoList } from "./components/TodoList";
+import Progress from "./components/Progress";
+
 
 function App() {
   return (
-    <div className="App">
-      <TodoList />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">TODO</Link>
+            </li>
+            <li>
+              <Link to="/progress">PROGRESS</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<TodoList />} />
+          <Route path="/progress" element={<Progress />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
 export default App;
---
+++ src/components/Progress.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Progress: React.FC = () => {
+  return <div className="progress">Progress</div>;
+};
+
+export default Progress;
