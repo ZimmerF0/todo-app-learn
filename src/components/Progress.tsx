524e5f89c3c4ebf1c6d22c434fa671b5e7bf7b00
Kishkin Vitalii <dimpla30@gmail.com>
2025-12-25 22:27:41 +0300

    add develop branch

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Progress: React.FC = () => {
-  return <div className="progress">Progress</div>;
+  return <div className="progress">Progressssss</div>;
 };
 
 export default Progress;
