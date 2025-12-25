6fd956d550fbae59c5c39647fdafcc4c093c36a6
Kishkin Vitalii <dimpla30@gmail.com>
2025-12-25 22:42:17 +0300

    jfejfk

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Progress: React.FC = () => {
-  return <div className="progress">Progresss</div>;
+  return <div className="progress">Progresssfvev</div>;
 };
 
 export default Progress;
