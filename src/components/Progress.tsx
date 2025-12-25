88ab3c0feafdf9b425aa562fbc100ef97aad8b4b
Kishkin Vitalii <dimpla30@gmail.com>
2025-12-25 22:38:40 +0300

    add develop featurewed

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Progress: React.FC = () => {
-  return <div className="progress">Progresss</div>;
+  return <div className="progress">Progresss njfcnjef</div>;
 };
 
 export default Progress;
