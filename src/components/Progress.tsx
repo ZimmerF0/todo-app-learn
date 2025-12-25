bedb79fb7e714ce18fffc307617c2afeded04f85
Kishkin Vitalii <dimpla30@gmail.com>
2025-12-25 22:29:34 +0300

    add develop feature

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Progress: React.FC = () => {
-  return <div className="progress">Progressssss</div>;
+  return <div className="progress">Progresss</div>;
 };
 
 export default Progress;
